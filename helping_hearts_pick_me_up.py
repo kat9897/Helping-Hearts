bb090b69753a95c526bae4afd31bd6ed47ce321d
Katrina <61071011+kat9897@users.noreply.github.com>
2021-01-24 10:57:24 -0500

    Add files via upload

--- helping_hearts_pick_me_up.py
+++ helping_hearts_pick_me_up.py
@@ -4,58 +4,34 @@ import random
 import tkinter as tk
 from PIL import Image, ImageTk
 from playsound import playsound
-
-# change any of these constants to style and make it your own!
-WINDOW_TITLE = 'Helping Hearts - a pick me up'
-WINDOW_WIDTH = 500
-WINDOW_HEIGHT = 800
-IMG_HEIGHT = 400
-IMG_WIDTH = 400
-PINK_COLOR_HEX = '#dea5a4'
-
-
-# opens all images from folder activity and quote
-ALL_ACTIVITY = [str("activity/") + file for file in os.listdir("activity/")]
+import tkinter.font as font
+ALL_ACTIVITY = [str("activity/") + file for file in os.listdir("activity/")] # opens all images from folder activity and quote
 ALL_QUOTE= [str("quote/") + file for file in os.listdir("quote/")]
 
-
 class selfcare:
-
     def __init__(self, root):
         self.root = root
-
-        # collecting all the clothes
         self.activity_images = ALL_ACTIVITY
         self.quote_images = ALL_QUOTE
-
-        # first pictures for top and bottom
+        # first pictures
         self.activity_image_path = self.activity_images[0]
         self.quote_image_path = self.quote_images[0]
-
-        # creating 2 frames
-        self.activity_frame = tk.Frame(self.root, bg=PINK_COLOR_HEX)
-        self.quote_frame = tk.Frame(self.root, bg=PINK_COLOR_HEX)
-
-        # adding top
+        # 2 frames to display quote + actvitity 
+        self.activity_frame = tk.Frame(self.root, bg='#dea5a4')
+        self.quote_frame = tk.Frame(self.root, bg='#dea5a4')
+        # add the activity 
         self.activity_image_label = self.create_photo(self.activity_image_path, self.activity_frame)
         self.activity_image_label.pack(side=tk.TOP)
-
-        # adding bottom
+        # add the quote 
         self.quote_image_label = self.create_photo(self.quote_image_path, self.quote_frame)
         self.quote_image_label.pack(side=tk.TOP)
-
         self.create_background()
 
     def create_background(self):
-        # title and resize the window
-        self.root.title(WINDOW_TITLE)
-        self.root.geometry('{0}x{1}'.format(WINDOW_WIDTH, WINDOW_HEIGHT))
-
-        # create buttons
+        self.root.title('Helping Hearts - a pick me up')
+        self.root.geometry('{0}x{1}'.format(500, 850))
         self.create_buttons()
-
-        # add the initial clothes onto the screen
-        self.activity_frame.pack(fill=tk.BOTH, expand=tk.YES)
+        self.activity_frame.pack(fill=tk.BOTH, expand=tk.YES) #quote/activity on to screen
         self.quote_frame.pack(fill=tk.BOTH, expand=tk.YES)
 
     def create_buttons(self):
@@ -64,34 +40,28 @@ class selfcare:
 
     def create_photo(self, image, frame):
         activity_image_file = Image.open(image)
-        image = activity_image_file.resize((IMG_WIDTH, IMG_HEIGHT), Image.ANTIALIAS)
+        image = activity_image_file.resize((400, 400), Image.ANTIALIAS)
         photo = ImageTk.PhotoImage(image)
         image_label = tk.Label(frame, image=photo, anchor=tk.CENTER)
         image_label.image = photo
-
         return image_label
 
     def update_photo(self, new_image, image_label):
         new_image_file = Image.open(new_image)
-        image = new_image_file.resize((IMG_WIDTH, IMG_HEIGHT), Image.ANTIALIAS)
+        image = new_image_file.resize((400, 400), Image.ANTIALIAS)
         photo = ImageTk.PhotoImage(image)
         image_label.configure(image=photo)
         image_label.image = photo
 
     def create_combo(self):
-        # randomly select an outfit
+        # randomly select a combination 
         new_activity_index = random.randint(0, len(self.activity_images)-1)
         new_quote_index = random.randint(0, len(self.quote_images)-1)
-
-        # add the clothes onto the screen
+        # add them on to the screen
         self.update_photo(self.activity_images[new_activity_index], self.activity_image_label)
         self.update_photo(self.quote_images[new_quote_index], self.quote_image_label)
-
-
-
 
 if __name__ == '__main__':
     root = tk.Tk()
     app = selfcare(root)
-
     root.mainloop()
